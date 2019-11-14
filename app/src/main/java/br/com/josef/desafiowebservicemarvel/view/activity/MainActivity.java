f4b2164ec15cc386a69af05485a6d4bd8bb0f324
Josemar Moura <josemarsp@gmail.com>
2019-11-14 19:36:51 -0300

    atualizações

---
+++ app/src/main/java/br/com/josef/desafiowebservicemarvel/view/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.josef.desafiowebservicemarvel.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.josef.desafiowebservicemarvel.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

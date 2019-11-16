411450670445cc6f50e526d8bc769fb419a309b2
Josemar Moura <josemarsp@gmail.com>
2019-11-16 19:19:27 -0300

    Falta o Parcelable funcionar

--- Desafio Web Service Marvel/app/src/main/java/br/com/josef/desafiowebservicemarvel/view/activity/DetalheActivity.java
+++ Desafio Web Service Marvel/app/src/main/java/br/com/josef/desafiowebservicemarvel/view/activity/DetalheActivity.java
@@ -2,39 +2,42 @@ package br.com.josef.desafiowebservicemarvel.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
-
 import br.com.josef.desafiowebservicemarvel.R;
 import br.com.josef.desafiowebservicemarvel.model.pojos.Date;
 import br.com.josef.desafiowebservicemarvel.model.pojos.Result;
 
+import com.squareup.picasso.Picasso;
+
 public class DetalheActivity extends AppCompatActivity {
+
+    private ImageView imgBanner;
+    private ImageView imgComic;
+    private TextView txtTitulo;
+    private TextView txtDescricao;
+    private TextView txtPublished;
+    private TextView txtPrice;
+    private TextView txtPages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe);
 
-        ImageView imgBanner = findViewById(R.id.imgBanner);
-        ImageView imgComic = findViewById(R.id.imgComicDetalhe);
-        TextView txtTitulo = findViewById(R.id.txtComicTitle);
-        TextView txtDescricao = findViewById(R.id.txtComicDescription);
-        TextView txtPublished = findViewById(R.id.txtPublishedInfo);
-        TextView txtPrice = findViewById(R.id.txtPriceInfo);
-        TextView txtPages = findViewById(R.id.txtPagesInfo);
+        initViews();
 
         if (getIntent() != null) {
 
-            Result result = getIntent().getExtras().getParcelable("Result");
-            Toast.makeText(this, result.getTitle() + " Loaded!", Toast.LENGTH_SHORT).show();
+            Result result = getIntent().getParcelableExtra("Result");
+            Picasso.get().load(result.getThumbnail().getPath() + ".jpg").into(imgComic);
 
-            Picasso.get().load(result.getThumbnail().getPath()).into(imgComic);
+             // Toast.makeText(this, result.getTitle() + " Loaded!", Toast.LENGTH_SHORT).show();
             // Picasso.get().load(result.getThumbnail().getPath()+".jpg").into(imgBanner);
             txtTitulo.setText(result.getTitle());
             txtDescricao.setText(result.getDescription());
@@ -47,6 +50,17 @@ public class DetalheActivity extends AppCompatActivity {
 
 
         }
+
+    }
+
+    private void initViews() {
+        imgBanner = findViewById(R.id.imgBanner);
+        imgComic = findViewById(R.id.imgComicDetalhe);
+        txtTitulo = findViewById(R.id.txtComicTitle);
+        txtDescricao = findViewById(R.id.txtComicDescription);
+        txtPublished = findViewById(R.id.txtPublishedInfo);
+        txtPrice = findViewById(R.id.txtPriceInfo);
+        txtPages = findViewById(R.id.txtPagesInfo);
 
     }
 }
